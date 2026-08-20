91d556efcca4ec651fc21d4891fd6b7d09e163e3
stickgaitan13 <stickgaitan13@gmail.com>
2026-08-20 01:18:52 -0500

    Wire storage permission picker and native bridge into MainActivity

--- tools/native_printer_bridge/src/com/inkatoys/MainActivity.java
+++ tools/native_printer_bridge/src/com/inkatoys/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.content.Intent;
 import android.net.Uri;
 import android.view.View;
-import android.view.ViewGroup;
 import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
@@ -13,7 +12,9 @@ import android.webkit.WebView;
 
 public class MainActivity extends Activity {
     private static final int FILE_CHOOSER_REQ = 5173;
+    private static final int STORAGE_TREE_REQ = 6612;
     private WebView webView;
+    private StorageBridge storageBridge;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,14 +26,36 @@ public class MainActivity extends Activity {
         settings.setDomStorageEnabled(true);
         settings.setAllowUniversalAccessFromFileURLs(true);
         settings.setMediaPlaybackRequiresUserGesture(false);
+        storageBridge = new StorageBridge(this);
+        webView.addJavascriptInterface(storageBridge, "InkaStorage");
         webView.setWebChromeClient(new InkaChromeClient(this));
         setContentView(webView);
         webView.loadUrl("file:///android_asset/index.html");
+        webView.postDelayed(new Runnable() {
+            @Override public void run() {
+                if (storageBridge != null) storageBridge.requestInitialPermission();
+            }
+        }, 1200);
+    }
+
+    public WebView getWebView() { return webView; }
+
+    public void launchStorageTree() {
+        try {
+            Intent i = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
+            i.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION | Intent.FLAG_GRANT_PREFIX_URI_PERMISSION);
+            startActivityForResult(i, STORAGE_TREE_REQ);
+        } catch (Throwable ignored) {}
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == STORAGE_TREE_REQ) {
+            Uri uri = (resultCode == RESULT_OK && data != null) ? data.getData() : null;
+            if (storageBridge != null) storageBridge.onTreeSelected(uri);
+            return;
+        }
         if (requestCode != FILE_CHOOSER_REQ || webView == null) return;
         Object tag = webView.getTag();
         if (!(tag instanceof ValueCallback)) return;
